5095552661e5e09bea2016a438723b9709c893d2
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-03-24 07:26:13 +0530

    body parsser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
  const express = require("express");
+ const bodyParser=require("body-parser");
 
  const {PORT} =require('./config/serverConfig');                           //importing  
 
@@ -7,11 +8,13 @@
     //create  the express object
     const app=express();
 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));                      //middleware
+
     app.listen(PORT, () =>{
-        console.log(process.env);
+        //console.log(process.env);
         console.log(`Server started at ${PORT}`)
     })
  }
-
 
  setupAndStartServer();
