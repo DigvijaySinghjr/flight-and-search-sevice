fd3a51519b5fab55df36dccfe234bb5c7743182b
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-06-14 17:40:14 +0530

    Added flights create crud

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,4 +1,8 @@
+const AirplaneRepository = require('./airplane-repository');
 
 module.exports ={
-    CityRepository: require('./city-repository')
+    CityRepository: require('./city-repository'),
+    FlightRepository: require('./flight-repository'),
+    AirlineRepository: require('./airplane-repository'),
+    AirplaneRepository: require('./airplane-repository')
 }
